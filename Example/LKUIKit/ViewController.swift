8b07fd7651c12867e9b83ae44317a65fceacb092
zhulinhua <cnzlh@163.com>
2018-06-05 15:29:30 +0800

    add CRRefresh

--- Example/LKUIKit/ViewController.swift
+++ Example/LKUIKit/ViewController.swift
@@ -7,18 +7,54 @@
 //
 
 import UIKit
+import LKUIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var count: Int = 10
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        tableView.register(UINib.init(nibName: "RefreshCell", bundle: nil), forCellReuseIdentifier: "RefreshCell")
+        tableView.cr.addHeadRefresh(animator: NormalHeaderAnimator() as CRRefreshProtocol) { [weak self] in
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2, execute: {
+                self?.count = 10
+                self?.tableView.cr.endHeaderRefresh()
+                self?.tableView.cr.resetNoMore()
+                self?.tableView.reloadData()
+            })
+        }
     }
 
 }
 
+// MARK: - Table view data source
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return CGFloat.leastNormalMagnitude
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return CGFloat.leastNormalMagnitude
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 130
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RefreshCell", for: indexPath) as! RefreshCell
+        return cell
+    }
+}
